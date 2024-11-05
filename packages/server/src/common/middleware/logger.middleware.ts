d3827cee801782cbeaa4a6b8f148a26684e4205d
王捷 <har3dy@icloud.com>
2024-11-05 10:07:16 +0800

    fix(server): 修复login接口返回201

--- packages/server/src/common/middleware/logger.middleware.ts
+++ packages/server/src/common/middleware/logger.middleware.ts
@@ -1,6 +1,7 @@
 import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import size from 'lodash.size';
+import pc from 'picocolors';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
@@ -9,24 +10,33 @@ export class LoggerMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const { method, originalUrl, body, query, ip } = req;
 
+    const requestPrefix = pc.cyan(`Request`);
+
     this.logger.log(
-      `Request: ${method} ${originalUrl} ${ip}`,
+      `${requestPrefix}: ${method} ${originalUrl} ${ip}`,
       LoggerMiddleware.name,
     );
 
     if (size(body)) {
       const hasPassword = !!body.password;
       this.logger.log(
-        `Request body: ${JSON.stringify({ ...body, ...(hasPassword ? { password: '*****' } : {}) })}`,
+        `${requestPrefix} body: ${JSON.stringify({ ...body, ...(hasPassword ? { password: '*****' } : {}) })}`,
         LoggerMiddleware.name,
       );
     }
     if (size(query)) {
       this.logger.log(
-        `Request query: ${JSON.stringify(query)}`,
+        `${requestPrefix} query: ${JSON.stringify(query)}`,
         LoggerMiddleware.name,
       );
     }
+
+    res.once('close', () => {
+      this.logger.log(
+        `${pc.blue(`Response`)}: ${res.statusCode}`,
+        LoggerMiddleware.name,
+      );
+    });
 
     next();
   }
